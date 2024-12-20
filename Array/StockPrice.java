c4230fbb14327c9fccc32006a40b300e4df3f06e
nittinkumarhr <nnitgin@gmail.com>
2024-12-20 22:54:21 +0530

    Buy and Shell Stock

---
+++ Array/StockPrice.java
@@ -0,0 +1,25 @@
+import  java.util.*;
+public class StockPrice{
+	public static int StockProfit(int prices[]){
+		int buyPrice=Integer.MAX_VALUE;
+		int MaxProfit=0;
+		for(int i=0;i<prices.length;i++){//profit
+			if (buyPrice<prices[i]) {
+				int profit=prices[i] - buyPrice;// Today's Profit 
+
+				MaxProfit=Math.max(MaxProfit,profit);//  Global Profit 
+			}
+			else{
+				 buyPrice=prices[i];
+
+			}
+
+		}
+		return MaxProfit;
+	}
+	public static void main(String args[]){
+		System.out.println("Problem :- Buy and Shell Stocks  calculate TOtal profit :)\n");
+		int  arr[]={7,1,5,3,6,4};
+		System.out.println("Total Profit :) "+StockProfit(arr));
+	}
+}
