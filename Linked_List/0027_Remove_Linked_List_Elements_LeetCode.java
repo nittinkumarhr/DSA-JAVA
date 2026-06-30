97705c60706a140d0fa1bb1b8a7c6dd63ecc60d2
Nitin kumar <127714499+nittinkumarhr@users.noreply.github.com>
2026-06-30 22:24:04 +0530

    ✅ Add #27 Remove Linked List Elements - LeetCode [Unknown] — java

---
+++ Linked_List/0027_Remove_Linked_List_Elements_LeetCode.java
@@ -0,0 +1,37 @@
+/*
+ * Problem: #27 - Remove Linked List Elements - LeetCode
+ * Difficulty: Unknown
+ * Topic: linked-list, recursion
+ * Runtime: 1
+ms
+ * Memory: 47.28
+MB
+ * Date: 2026-06-30
+ * LeetCode: https://leetcode.com/problems/remove-linked-list-elements/
+ */
+
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode removeElements(ListNode head, int val) {
+        ListNode ans = new ListNode(0, head);
+        ListNode dummy = ans;
+
+        while (dummy != null) {
+            while (dummy.next != null && dummy.next.val == val) {
+                dummy.next = dummy.next.next;
+            }
+            dummy = dummy.next;
+        }
+
+        return ans.next;        
+    }
+}
