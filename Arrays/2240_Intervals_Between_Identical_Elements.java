13dbcf88e0686078d719dbb5aba6e3344aed4b39
Nitin kumar <127714499+nittinkumarhr@users.noreply.github.com>
2026-07-05 23:55:27 +0530

    ✅ Add #2240 Intervals Between Identical Elements [Medium] — Java

---
+++ Arrays/2240_Intervals_Between_Identical_Elements.java
@@ -0,0 +1,39 @@
+/*
+ * Problem: #2240 - Intervals Between Identical Elements
+ * Difficulty: Medium
+ * Topic: array, hash-table, prefix-sum
+ * Runtime: 170 ms
+ * Memory: 189.3 MB
+ * Date: 05 Jul 2026
+ * LeetCode: https://leetcode.com/problems/intervals-between-identical-elements/
+ */
+
+class Solution {
+    public long[] getDistances(int[] arr) {
+        long[] output = new long[arr.length];
+        Map<Integer, Long> sumMap = new HashMap<>();
+        Map<Integer, Integer> countMap = new HashMap<>();
+        for (int i = 0; i < arr.length; ++ i) {
+            if (!sumMap.containsKey(arr[i])) {
+                sumMap.put(arr[i], 0l);
+                countMap.put(arr[i], 0);
+            }
+            
+            output[i] += i * (long)countMap.get(arr[i]) - sumMap.get(arr[i]);
+            sumMap.put(arr[i], sumMap.get(arr[i]) + i);
+            countMap.put(arr[i], countMap.get(arr[i]) + 1);
+        }
+        
+        sumMap = new HashMap<>();
+        countMap = new HashMap<>();
+        int len = arr.length;
+        for (int i = len - 1; i >= 0; -- i) {
+            if (!sumMap.containsKey(arr[i])) {
+                sumMap.put(arr[i], 0l);
+                countMap.put(arr[i], 0);
+            }
+            
+            output[i] += (len - i - 1) * (long)countMap.get(arr[i]) - sumMap.get(arr[i]);
+            sumMap.put(arr[i], sumMap.get(arr[i]) + (len - i - 1));
+            countMap.put(arr[i], countMap.get(arr[i]) + 1);
+        }
