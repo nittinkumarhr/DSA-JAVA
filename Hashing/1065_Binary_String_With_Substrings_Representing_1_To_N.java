1da38ffc5d087003bb7f365aa92b04d4d5c548df
Nitin kumar <127714499+nittinkumarhr@users.noreply.github.com>
2026-07-08 23:17:43 +0530

    ✅ Add #1065 Binary String With Substrings Representing 1 To N [Medium] — Java

--- Hashing/1065_Binary_String_With_Substrings_Representing_1_To_N.java
+++ Hashing/1065_Binary_String_With_Substrings_Representing_1_To_N.java
@@ -3,26 +3,12 @@
  * Difficulty: Medium
  * Topic: hash-table, string, bit-manipulation, sliding-window
  * Runtime: 0 ms
- * Memory: 42.6 MB
+ * Memory: 42.8 MB
  * Date: 08 Jul 2026
  * LeetCode: https://leetcode.com/problems/binary-string-with-substrings-representing-1-to-n/
  */
 
 class Solution {
-    public String binary(int n){
-        String res = "";
-        while(n>0){
-            int t = n%2;
-            if(t == 0){
-                res +="0";
-            }
-            else{
-                res +="1";
-            }
-            n = n/2;
-        }
-        return res;
-    }
     public boolean queryString(String s, int n) {
         for(int i =1;i<=n;i++){
             String bin = Integer.toBinaryString(i);
