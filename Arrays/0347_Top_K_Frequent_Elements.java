b76d6afd41ca35e4d23be16afc902ec2c148e8d6
Nitin kumar <127714499+nittinkumarhr@users.noreply.github.com>
2026-07-10 23:50:58 +0530

    ✅ Add #347 Top K Frequent Elements [Medium] — Java

--- Arrays/0347_Top_K_Frequent_Elements.java
+++ Arrays/0347_Top_K_Frequent_Elements.java
@@ -2,8 +2,8 @@
  * Problem: #347 - Top K Frequent Elements
  * Difficulty: Medium
  * Topic: array, hash-table, divide-and-conquer, sorting, heap-priority-queue, bucket-sort, counting, quickselect
- * Runtime: 15 ms
- * Memory: 47.7 MB
+ * Runtime: 16 ms
+ * Memory: 47.1 MB
  * Date: 10 Jul 2026
  * LeetCode: https://leetcode.com/problems/top-k-frequent-elements/
  */
