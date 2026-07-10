2cd38d84eaef4d59b94d7123d6965fbb3af818c4
Nitin kumar <127714499+nittinkumarhr@users.noreply.github.com>
2026-07-10 17:21:15 +0530

    ✅ Add #347 Top K Frequent Elements [Medium] — Java

---
+++ Arrays/0347_Top_K_Frequent_Elements.java
@@ -0,0 +1,33 @@
+/*
+ * Problem: #347 - Top K Frequent Elements
+ * Difficulty: Medium
+ * Topic: array, hash-table, divide-and-conquer, sorting, heap-priority-queue, bucket-sort, counting, quickselect
+ * Runtime: 16 ms
+ * Memory: 47.5 MB
+ * Date: 10 Jul 2026
+ * LeetCode: https://leetcode.com/problems/top-k-frequent-elements/
+ */
+
+class Solution {
+    public int[] topKFrequent(int[] nums, int k) {
+
+        HashMap<Integer,Integer> map = new HashMap<>();
+
+        for(int element : nums){
+            map.put(element, map.getOrDefault(element,0)+1);
+        }
+
+        ArrayList<Map.Entry<Integer,Integer>> arr =
+                new ArrayList<>(map.entrySet());
+
+        arr.sort((a,b) -> b.getValue() - a.getValue());
+
+        int[] last = new int[k];
+
+        for(int i = 0; i < k; i++){
+            last[i] = arr.get(i).getKey();
+        }
+
+        return last;
+    }
+}
