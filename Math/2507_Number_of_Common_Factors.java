b254e35f4717582f4e56126acc181696400b8c72
Nitin kumar <127714499+nittinkumarhr@users.noreply.github.com>
2026-07-15 20:06:12 +0530

    ✅ Add #2507 Number of Common Factors [Easy] — Java

---
+++ Math/2507_Number_of_Common_Factors.java
@@ -0,0 +1,26 @@
+/*
+ * Problem: #2507 - Number of Common Factors
+ * Difficulty: Easy
+ * Topic: math, enumeration, number-theory
+ * Runtime: 1 ms
+ * Memory: 42 MB
+ * Date: 15 Jul 2026
+ * LeetCode: https://leetcode.com/problems/number-of-common-factors/
+ */
+
+class Solution {
+    public int commonFactors(int a, int b) {
+    int n =0;
+    for(int i=1;i<=s;i++){
+        if(a%i ==0 && b%i == 0){
+    }
+            n++;
+        }
+    return n;
+    if(a>b){
+    }
+    }
+    int s;
+        s=a;
+    else{s=b;}
+}
