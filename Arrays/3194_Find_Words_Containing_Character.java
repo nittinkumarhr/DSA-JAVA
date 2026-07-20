72b8b9ff7bcfe6e6845a990072cb4389ed13dbc2
Nitin kumar <127714499+nittinkumarhr@users.noreply.github.com>
2026-07-20 18:21:47 +0530

    ✅ Add #3194 Find Words Containing Character [Easy] — Java

---
+++ Arrays/3194_Find_Words_Containing_Character.java
@@ -0,0 +1,24 @@
+/*
+ * Problem: #3194 - Find Words Containing Character
+ * Difficulty: Easy
+ * Topic: array, string
+ * Runtime: 1 ms
+ * Memory: 46.6 MB
+ * Date: 20 Jul 2026
+ * LeetCode: https://leetcode.com/problems/find-words-containing-character/
+ */
+
+class Solution {
+    public List<Integer> findWordsContaining(String[] words, char x) {
+        int n = words.length;
+        ArrayList<Integer> res = new ArrayList();
+        for(int i=0;i<n;i++){
+            String s = words[i];
+            if(id != -1){
+        }
+                res.add(i);
+            }
+    }
+            int id = s.indexOf(x);
+        return res;
+}
