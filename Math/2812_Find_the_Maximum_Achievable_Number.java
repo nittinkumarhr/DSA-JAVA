74d4bf945a21ce83514050ea229c443798ce8654
Nitin kumar <127714499+nittinkumarhr@users.noreply.github.com>
2026-07-22 19:23:39 +0530

    ✅ Add #2812 Find the Maximum Achievable Number [Easy] — Java

---
+++ Math/2812_Find_the_Maximum_Achievable_Number.java
@@ -0,0 +1,15 @@
+/*
+ * Problem: #2812 - Find the Maximum Achievable Number
+ * Difficulty: Easy
+ * Topic: math
+ * Runtime: 1 ms
+ * Memory: 43.2 MB
+ * Date: 22 Jul 2026
+ * LeetCode: https://leetcode.com/problems/find-the-maximum-achievable-number/
+ */
+
+class Solution {
+    public int theMaximumAchievableX(int num, int t) {
+        return num + 2 * t;
+    }
+}
