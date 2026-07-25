580d8ddb1c7afa6d7835bddeb93c2591f5879076
Nitin kumar <127714499+nittinkumarhr@users.noreply.github.com>
2026-07-26 02:20:15 +0530

    ✅ Add #867808 New Year Resolution Practice Coding Problem [Difficulty:19] — Java

---
+++ Other/867808_New_Year_Resolution_Practice_Coding_Problem.java
@@ -0,0 +1,28 @@
+/*
+ * Problem: #867808 - New Year Resolution Practice Coding Problem
+ * Difficulty: Difficulty:19
+ * Topic: Other
+ * Runtime: N/A
+ * Memory: N/A
+ * Date: 26 Jul 2026
+ * CodeChef: https://www.codechef.com/problems/NYRES
+ */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+import java.util.Scanner;
+class Codechef
+{
+    public static void main (String[] args) throws java.lang.Exception
+    {
+        // your code goes here
+         Scanner sc = new Scanner(System.in);
+        if (sc.hasNextInt()) {
+            int x = sc.nextInt();
+            System.out.println(x * 31);
+        }
+        sc.close();
+
+    }
+}
