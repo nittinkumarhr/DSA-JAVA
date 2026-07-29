18ad43fa27473761dc48c50c02e23d05eba8cebd
Nitin kumar <127714499+nittinkumarhr@users.noreply.github.com>
2026-07-30 02:31:57 +0530

    ✅ Add #873422 Distinct Companies Practice Coding Problem [Unknown] — SQL

---
+++ Other/873422_Distinct_Companies_Practice_Coding_Problem.sql
@@ -0,0 +1,10 @@
+// Problem: #873422 - Distinct Companies Practice Coding Problem
+// Difficulty: Unknown
+// Topic: Other
+// Runtime: N/A
+// Memory: N/A
+// Date: 30 Jul 2026
+// CodeChef: https://www.codechef.com/problems/SQLPBP13
+
+-- your code goes here
+select distinct  company_name from Works;
