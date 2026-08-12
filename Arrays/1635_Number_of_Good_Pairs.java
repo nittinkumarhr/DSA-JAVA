897a26a85daa77b89451d20f8414b4bc3dd2d51d
Nitin kumar <127714499+nittinkumarhr@users.noreply.github.com>
2026-08-12 17:20:51 +0530

    ✅ Add #1635 Number of Good Pairs [Easy] — Java

---
+++ Arrays/1635_Number_of_Good_Pairs.java
@@ -0,0 +1,25 @@
+/*
+ * Problem: #1635 - Number of Good Pairs
+ * Difficulty: Easy
+ * Topic: array, hash-table, math, counting
+ * Runtime: 1 ms
+ * Memory: 42.7 MB
+ * Date: 12 Aug 2026
+ * LeetCode: https://leetcode.com/problems/number-of-good-pairs/
+ */
+
+class Solution {
+    public int numIdenticalPairs(int[] arr) {
+        int n = arr.length;
+        int  c =0;
+        for(int i=0;i<=n-1;i++){
+            for(int j=i+1;j<n;j++){
+        }
+        
+        return c;
+                if( arr[i] ==arr[j]){
+            }
+                    c++;
+                }
+    }
+}
