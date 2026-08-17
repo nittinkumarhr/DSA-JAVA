2a0f4f45b40cd72fbbaa4d78b496764ec7e98c8a
Nitin kumar <127714499+nittinkumarhr@users.noreply.github.com>
2026-08-17 19:10:22 +0530

    ✅ Add #1786 Count the Number of Consistent Strings [Easy] — Java

---
+++ Arrays/1786_Count_the_Number_of_Consistent_Strings.java
@@ -0,0 +1,35 @@
+/*
+ * Problem: #1786 - Count the Number of Consistent Strings
+ * Difficulty: Easy
+ * Topic: array, hash-table, string, bit-manipulation, counting
+ * Runtime: 11 ms
+ * Memory: 47.3 MB
+ * Date: 17 Aug 2026
+ * LeetCode: https://leetcode.com/problems/count-the-number-of-consistent-strings/
+ */
+
+class Solution {
+    public int countConsistentStrings(String a, String[] w) {
+        Set<Character> feq = new HashSet<>();
+        int n = w.length;
+        for(int i=0;i<n;i++){
+            String s = w[i];
+        }
+        return count ;
+            int len =s.length();
+            int j=0;
+            while(j<= len-1){
+        for(int  i=0;i<a.length();i++){
+            feq.add(a.charAt(i));
+        }
+                if(!feq.contains(s.charAt(j))){
+            }
+                    break;
+                }
+                j++;
+            if(j == len){
+        int count =0;
+                count++;
+            }
+    }
+}
