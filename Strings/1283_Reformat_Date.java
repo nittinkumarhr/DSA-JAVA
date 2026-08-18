f50480648a2ca3c59887b65a9b2325a6e7670f84
Nitin kumar <127714499+nittinkumarhr@users.noreply.github.com>
2026-08-18 10:46:30 +0530

    ✅ Add #1283 Reformat Date [Easy] — Java

---
+++ Strings/1283_Reformat_Date.java
@@ -0,0 +1,40 @@
+/*
+ * Problem: #1283 - Reformat Date
+ * Difficulty: Easy
+ * Topic: string
+ * Runtime: 2 ms
+ * Memory: 43 MB
+ * Date: 18 Aug 2026
+ * LeetCode: https://leetcode.com/problems/reformat-date/
+ */
+
+of day, month and year
+        String[] dates = date.split(" ");
+        
+        //take StringBuilder as String is immutable in Java
+        StringBuilder formatedDate = new StringBuilder();
+        
+        //append year as it is
+        formatedDate.append(dates[2]+"-");
+        
+        //append month using map
+        formatedDate.append(months.get(dates[1])+"-");
+        //split the string date based on space so that we can get three separate strings 
+        months.put("Dec","12");
+        months.put("Nov","11");
+        months.put("Oct","10");
+        months.put("Sep","09");
+        
+        //fetch day by removing last 2 characters
+        String day = dates[0].substring(0,dates[0].length()-2);
+        
+        // if day is a single digit, then need to append 0 first then the day else append 
+        day as it is
+        if(day.length() == 1)
+            formatedDate.append("0"+day);
+        else formatedDate.append(day);
+                            
+        return formatedDate.toString();       
+        
+    }
+}
