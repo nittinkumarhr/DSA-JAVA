0d4232f66cb674ea453220cab02ae0b31089626a
Nitin kumar <127714499+nittinkumarhr@users.noreply.github.com>
2026-08-21 22:31:38 +0530

    ✅ Add #2210 Find Target Indices After Sorting Array [Easy] — Java

--- Arrays/2210_Find_Target_Indices_After_Sorting_Array.java
+++ Arrays/2210_Find_Target_Indices_After_Sorting_Array.java
@@ -2,8 +2,8 @@
  * Problem: #2210 - Find Target Indices After Sorting Array
  * Difficulty: Easy
  * Topic: array, binary-search, sorting
- * Runtime: 6 ms
- * Memory: 50.1 MB
+ * Runtime: 0 ms
+ * Memory: 44.2 MB
  * Date: 21 Aug 2026
  * LeetCode: https://leetcode.com/problems/find-target-indices-after-sorting-array/
  */
@@ -11,13 +11,19 @@
 class Solution {
     public List<Integer> targetIndices(int[] arr, int target) {
         List<Integer> res = new ArrayList();
-        Arrays.sort(arr);
+        int c =0;int lesthanTar =0;
         int n = arr.length;
         for(int i =0;i<n;i++){
         return res;
             if(arr[i] == target){
         }
-                res.add(i);
+                c++;
             }
+            else if( arr[i] < target){
+                lesthanTar++;
+            }
+        for(int i =0;i<c;i++){
+            res.add(lesthanTar++);
+        }
     }
 }
